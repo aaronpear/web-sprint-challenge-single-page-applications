20d4f9fdb89fb94282d83617e7226ff73c6e56e9
Aaron Reyes <56135909+aaronpear@users.noreply.github.com>
2021-11-12 12:02:18 -0800

    Form validation done

--- src/components/OrderForm.js
+++ src/components/OrderForm.js
@@ -1,7 +1,15 @@
 import React, { useState } from "react";
 import { useHistory } from 'react-router-dom';
+import * as yup from 'yup';
 
 const OrderForm = () => {
+    const schema = yup.object().shape({
+        name: yup
+            .string()
+            .trim()
+            .min(2, 'name must be at least 2 characters')
+    })
+    
     const initialFormValues = {
         name: '',
         size: '',
@@ -9,7 +17,13 @@ const OrderForm = () => {
         topping2: false,
         special: ''
     }
+
+    const initialFormErrors = {
+        name: ''
+    }
+
     const [formValues, setFormValues] = useState(initialFormValues);
+    const [formErrors, setFormErrors] = useState(initialFormErrors);
 
     const history = useHistory();
 
@@ -19,12 +33,20 @@ const OrderForm = () => {
 
     const inputChange = (event) => {
         const { name, value } = event.target;
+        yup.reach(schema, name).validate(value)
+            .then(() => setFormErrors({ ...formErrors, [name]: '' }))
+            .catch(error => setFormErrors({ ...formErrors, [name]: error.errors[0]}))
         setFormValues({ ...formValues, [name]: value });
     }
 
     return (
         <div>
             <form id='pizza-form'>
+                <div className='errors'>
+                    <div>
+                        {formErrors.name}
+                    </div> 
+                </div>
                 <label>Name:
                     <input
                         value={formValues.name}
